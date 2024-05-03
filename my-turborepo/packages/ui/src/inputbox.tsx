650960f04c84b0febd8334df6c4c8478e5b8f20e
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-03 21:07:39 +0500

    Updated ui folder

---
+++ my-turborepo/packages/ui/src/inputbox.tsx
@@ -0,0 +1,9 @@
+export const Inputbox = () => {
+  return (
+    <div>
+      <h1>Inputbox Component</h1>
+      <input type="text">Enter text</input>
+      <h1>Inputbox Component</h1>
+    </div>
+  );
+};
