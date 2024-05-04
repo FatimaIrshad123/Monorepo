a1cd973edf3fe23144e1971443c5cc6d99922ef2
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-05-04 18:23:50 +0500

    Updated backend code

--- my-turborepo/apps/backend/index.ts
+++ my-turborepo/apps/backend/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import {VALUE} from '@repo/common'
+import {VALUE} from '@repo/common/config'
 
 const app = express()
 const port = 4000;
 
+console.log(VALUE)
+
 app.get('/',(req:any,res:any) => {
+    console.log(VALUE)
     res.json({msg:'Hello from fatima'})
 })
 app.listen(port, () => console.log(`App running on port ${port}`))
